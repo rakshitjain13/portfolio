bb3c16b6af7caa1da95d80957cc81e7e2de5ceb6
Rakshit Jain <54314936+rakshitjain13@users.noreply.github.com>
2022-10-02 15:37:09 +0530

    About update (#2)
    * intial update
    
    * added resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,73 +1,88 @@
-import React from 'react'
-import './about.css';
-import Rakshitimg from '../0-removebg-preview.png';
+import React from "react";
+import "./about.css";
+import Rakshitimg from "../0-removebg-preview.png";
 export default function About() {
-    return (
-			<div>
-				<section id="about" class="about">
-					<div class="container" data-aos="fade-up">
-						<div class="section-title">
-							<h2>About</h2>
-							<p>
-								Hello! I'm Rakshit Jain,studying Computer Science Engineering
-								(2ND Year) from{" "}
-								<strong>Indian Institue of Information Technology,Pune.</strong>
-							</p>
-						</div>
+  return (
+    <div>
+      <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <h2>About</h2>
+            <p>
+              2023 Computer Science Engineering undergraduate from Indian
+              Institute of Information Technology , Pune having a keen
+              proficieny in mern stack development with adequate Data Structures
+              and Algorithms, and interest to learn and make impactful projects
+              that Elon musk didn’t do.
+            </p>
+          </div>
 
-						<div class="row">
-							<div class="col-lg-4">
-								<img src={Rakshitimg} class="img-fluid" alt="Rakshit Jain" />
-							</div>
-							<div class="col-lg-8 pt-4 pt-lg-0 content mt-5">
-								<h3>Web Developer &amp; Competitve Programmer</h3>
-								<p class="font-italic"></p>
-								<div class="row">
-									<div class="col-lg-6 ">
-										<ul>
-											<li>
-												<i class="fa fa-angle-right"></i>{" "}
-												<strong>Birthday:</strong> 3 March 2001
-											</li>
-											<li>
-												<i class="fa fa-angle-right"></i>{" "}
-												<strong>Website:</strong> www.example.com
-											</li>
-											<li>
-												<i class="fa fa-angle-right"></i> <strong>City:</strong>{" "}
-												Mumbai,Maharashtra,India
-											</li>
-										</ul>
-									</div>
-									<div class="col-lg-6">
-										<ul>
-											<li>
-												<i class="fa fa-angle-right"></i>{" "}
-												<strong>Degree:</strong> B.Tech in CSE
-											</li>
-											<li>
-												<i class="fa fa-angle-right"></i>{" "}
-												<strong>Email:</strong> rakshitdhoka13@gmail.com
-											</li>
-											<li>
-												<i class="fa fa-angle-right"></i>{" "}
-												<strong>Freelance:</strong> Available
-											</li>
-										</ul>
-									</div>
-								</div>
-								<p>
-			
-										Started my journey with C++ with evolution till OOPS.I
-										always seek for what is behind the scences which lead me to
-										a Full Stack MERN Developer.Currently engaged in DSA and CP
-										Visualizer for all programmers(MERN + Python).
-	
-							</p>
-							</div>
-						</div>
-					</div>
-				</section>
-			</div>
-		);
+          <div class="row">
+            <div class="col-lg-4">
+              <img src={Rakshitimg} class="img-fluid" alt="Rakshit Jain" />
+            </div>
+
+            <div class="col-lg-8 pt-4 pt-lg-0 content mt-5">
+              <h3>Full Web Developer &amp; Competitve Programmer</h3>
+              <p class="font-italic"></p>
+              <div>
+                <div class="row">
+                  <div class="col-lg-6 ">
+                    <ul>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Current Company :</strong> Yugabyte
+                      </li>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Birthday:</strong> 3 March 2001
+                      </li>
+
+                      <li>
+                        <i class="fa fa-angle-right"></i> <strong>City:</strong>{" "}
+                        Mumbai,Maharashtra,India
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-6">
+                    <ul>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Current Role:</strong> Software development
+                        intern
+                      </li>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Degree:</strong> B.Tech in CSE
+                      </li>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Email:</strong> rakshitdhoka13@gmail.com
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="d-flex ">
+                  <a
+                    href="https://drive.google.com/file/d/1wt8HMaYF0BM0MOA2TPxtrS31PR55OYZr/view?usp=sharing"
+                    target="_blank"
+                    className=" btn btn-lg rounded"
+                    style={{
+                      backgroundColor: "#c060a1",
+                      borderColor: "#c060a1",
+                      fontWeight: "600",
+                    }}
+                    rel="noreferrer"
+                  >
+                    Resume
+                    <i class="fa fa-download ml-2" aria-hidden="true"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
