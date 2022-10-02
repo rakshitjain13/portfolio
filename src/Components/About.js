08ed7034f168d6173f2706c7097eb4bb00943cad
Rakshit Jain <rjain@yugabyte.com>
2022-10-02 15:35:34 +0530

    added resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,45 +21,64 @@ export default function About() {
             <div class="col-lg-4">
               <img src={Rakshitimg} class="img-fluid" alt="Rakshit Jain" />
             </div>
+
             <div class="col-lg-8 pt-4 pt-lg-0 content mt-5">
               <h3>Full Web Developer &amp; Competitve Programmer</h3>
               <p class="font-italic"></p>
-              <div class="row">
-                <div class="col-lg-6 ">
-                  <ul>
-                    <li>
-                      <i class="fa fa-angle-right"></i>{" "}
-                      <strong>Current Company :</strong> Yugabyte
-                    </li>
-                    <li>
-                      <i class="fa fa-angle-right"></i>{" "}
-                      <strong>Birthday:</strong> 3 March 2001
-                    </li>
+              <div>
+                <div class="row">
+                  <div class="col-lg-6 ">
+                    <ul>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Current Company :</strong> Yugabyte
+                      </li>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Birthday:</strong> 3 March 2001
+                      </li>
 
-                    <li>
-                      <i class="fa fa-angle-right"></i> <strong>City:</strong>{" "}
-                      Mumbai,Maharashtra,India
-                    </li>
-                  </ul>
+                      <li>
+                        <i class="fa fa-angle-right"></i> <strong>City:</strong>{" "}
+                        Mumbai,Maharashtra,India
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-6">
+                    <ul>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Current Role:</strong> Software development
+                        intern
+                      </li>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Degree:</strong> B.Tech in CSE
+                      </li>
+                      <li>
+                        <i class="fa fa-angle-right"></i>{" "}
+                        <strong>Email:</strong> rakshitdhoka13@gmail.com
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                <div class="col-lg-6">
-                  <ul>
-                    <li>
-                      <i class="fa fa-angle-right"></i>{" "}
-                      <strong>Current Role:</strong> Software development intern
-                    </li>
-                    <li>
-                      <i class="fa fa-angle-right"></i> <strong>Degree:</strong>{" "}
-                      B.Tech in CSE
-                    </li>
-                    <li>
-                      <i class="fa fa-angle-right"></i> <strong>Email:</strong>{" "}
-                      rakshitdhoka13@gmail.com
-                    </li>
-                  </ul>
+                <div className="d-flex ">
+                  <a
+                    href="https://drive.google.com/file/d/1wt8HMaYF0BM0MOA2TPxtrS31PR55OYZr/view?usp=sharing"
+                    target="_blank"
+                    className=" btn btn-lg rounded"
+                    style={{
+                      backgroundColor: "#c060a1",
+                      borderColor: "#c060a1",
+                      fontWeight: "600",
+                    }}
+                    rel="noreferrer"
+                  >
+                    Resume
+                    <i class="fa fa-download ml-2" aria-hidden="true"></i>
+                  </a>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
